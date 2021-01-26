8704fe9c1f40270d6940cba1a8554b594b27a280
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-26 22:49:42 +0700

    add select input

---
+++ ShopOnline.API/Controllers/ProductBrandsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities.Products;
+using ShopOnline.API.Models;
+
+namespace ShopOnline.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductBrandsController : ControllerBase
+    {
+        private readonly ShopOnlineContext _context;
+
+        public ProductBrandsController(ShopOnlineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductBrands
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductBrand>>> GetProductBrands()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+
+        // GET: api/ProductBrands/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductBrand>> GetProductBrand(int id)
+        {
+            var productBrand = await _context.ProductBrands.FindAsync(id);
+
+            if (productBrand == null)
+            {
+                return NotFound();
+            }
+
+            return productBrand;
+        }
+
+        // PUT: api/ProductBrands/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductBrand(int id, ProductBrand productBrand)
+        {
+            if (id != productBrand.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productBrand).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductBrandExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductBrands
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductBrand>> PostProductBrand(ProductBrand productBrand)
+        {
+            _context.ProductBrands.Add(productBrand);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductBrand", new { id = productBrand.Id }, productBrand);
+        }
+
+        // DELETE: api/ProductBrands/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductBrand(int id)
+        {
+            var productBrand = await _context.ProductBrands.FindAsync(id);
+            if (productBrand == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductBrands.Remove(productBrand);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductBrandExists(int id)
+        {
+            return _context.ProductBrands.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ShopOnline.API/Controllers/ProductPricesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities.Products;
+using ShopOnline.API.Models;
+
+namespace ShopOnline.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductPricesController : ControllerBase
+    {
+        private readonly ShopOnlineContext _context;
+
+        public ProductPricesController(ShopOnlineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductPrices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductPrice>>> GetProductPrices()
+        {
+            return await _context.ProductPrices.ToListAsync();
+        }
+
+        // GET: api/ProductPrices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductPrice>> GetProductPrice(int id)
+        {
+            var productPrice = await _context.ProductPrices.FindAsync(id);
+
+            if (productPrice == null)
+            {
+                return NotFound();
+            }
+
+            return productPrice;
+        }
+
+        // PUT: api/ProductPrices/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductPrice(int id, ProductPrice productPrice)
+        {
+            if (id != productPrice.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productPrice).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductPriceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductPrices
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductPrice>> PostProductPrice(ProductPrice productPrice)
+        {
+            _context.ProductPrices.Add(productPrice);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductPriceExists(productPrice.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetProductPrice", new { id = productPrice.Id }, productPrice);
+        }
+
+        // DELETE: api/ProductPrices/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductPrice(int id)
+        {
+            var productPrice = await _context.ProductPrices.FindAsync(id);
+            if (productPrice == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductPrices.Remove(productPrice);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductPriceExists(int id)
+        {
+            return _context.ProductPrices.Any(e => e.Id == id);
+        }
+    }
+}
